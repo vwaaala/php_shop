0c7b9129776d1f784873d47606183c19d6b66852
Rafiqul Islam <vwaaala@gmail.com>
2022-04-23 01:51:53 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+// Attach common header section
+include('templates/header.php');
+
+// Attach banner section
+include('templates/banner.php');
+
+// Attach top sale section
+include ('templates/top-sale.php');
+
+// Attach special price section
+include ('templates/special-price.php');
+
+// Attach banner ads section
+include ('templates/banner-ads.php');
+
+// Attach new phones section
+include ('templates/new-phones.php');
+
+// Attach blogs section
+include ('templates/blogs.php');
+
+// Attach common footer file section
+include('templates/footer.php');
+
---
+++ templates/banner-ads.php
@@ -0,0 +1,8 @@
+<!-- Banner Ads  -->
+<section id="banner_adds">
+    <div class="container py-5 text-center">
+        <img src="../static/image/banner1-cr-500x150.jpg" alt="banner1" class="img-fluid">
+        <img src="../static/image/banner2-cr-500x150.jpg" alt="banner1" class="img-fluid">
+    </div>
+</section>
+<!-- !Banner Ads  -->
